22533208a3d0a95296c55e1798885583e0e4560f
Uriel Jimenez <ujimenez@griddynamics.com>
2022-09-21 00:13:12 -0500

    Initial commit

---
+++ src/main/java/com/reactiveparadigm/reactiveParadigm/ReactiveParadigmApplication.java
@@ -0,0 +1,13 @@
+package com.reactiveparadigm.reactiveParadigm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveParadigmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveParadigmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactiveparadigm/reactiveParadigm/ReactiveParadigmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactiveparadigm.reactiveParadigm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveParadigmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
